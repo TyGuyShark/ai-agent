9e5c1e1f620648e32002c82f44aa5cd3dbd81921
tzehfus <tzehfus@gmail.com>
2025-07-09 06:26:06 +0900

    B: Updated main.py to accept an argument as a query

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,11 +8,25 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+
+    print(f"Script name: {sys.argv[0]}")
+    print(f"# of args w/ script name: {len(sys.argv)}")
+
+    prompt = ""
+    if len(sys.argv) > 1:
+        prompt += sys.argv[1]
+        for arg in sys.argv[2:]:
+            prompt += " " + arg
+        print(f"Prompt: {prompt}")
+    else:
+        print("No prompts (arguments) provided. Exiting with Code 1.")
+        sys.exit(1)
+
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    #print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    #print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
