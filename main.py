72e8c1533dd65227241cf2cafd6bb806f5a2c74a
tzehfus <tzehfus@gmail.com>
2025-07-10 06:55:28 +0900

    C: Updated main.py with --verbose

--- main.py
+++ main.py
@@ -2,31 +2,33 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    print(f"Script name: {sys.argv[0]}")
-    print(f"# of args w/ script name: {len(sys.argv)}")
 
-    prompt = ""
+    user_prompt = ""
     if len(sys.argv) > 1:
-        prompt += sys.argv[1]
-        for arg in sys.argv[2:]:
-            prompt += " " + arg
-        print(f"Prompt: {prompt}")
+        user_prompt += sys.argv[1]
     else:
         print("No prompts (arguments) provided. Exiting with Code 1.")
         sys.exit(1)
 
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
+    print()
     print(response.text)
-    #print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    #print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
