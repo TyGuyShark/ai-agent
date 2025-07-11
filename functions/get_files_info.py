d04293f81c730c9c3d597295c8c93675f8537219
tzehfus <tzehfus@gmail.com>
2025-07-12 08:21:06 +0900

    E: Updated get_files_info to pull file info and errors and created tests.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,25 @@
+import os
+
+
 def get_files_info(working_directory, directory=None):
+    full_path = os.path.join(working_directory, directory)
+    working_dir_abs = os.path.abspath(working_directory)
+    target_path_abs = os.path.abspath(full_path)
+    if not target_path_abs.startswith(working_dir_abs):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_path_abs):
+        return f'Error: "{directory}" is not a directory'
+
+    dir_content_str = ""
+
+    try:
+        for obj in os.listdir(full_path):
+            full_obj_path = os.path.join(full_path, obj)
+            file_size = os.path.getsize(full_obj_path)
+            is_dir = os.path.isdir(full_obj_path)
+            dir_content_str += f'- {obj}: file_size={file_size} bytes, is_dir={is_dir}\n'
+    except Exception as e:
+        error_message = str(e)
+        return f'Error: {error_message}'
+
+    return dir_content_str
