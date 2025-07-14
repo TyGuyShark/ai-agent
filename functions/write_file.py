574f1d58143955a3921a62e7330ea5aae0336a28
tzehfus <tzehfus@gmail.com>
2025-07-15 05:39:01 +0900

    G: Created write_file.py, modified tests.py

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,1 +1,22 @@
+import os
+
+
 def write_file(working_directory, file_path, content):
+    full_path = os.path.join(working_directory, file_path)
+    working_dir_abs = os.path.abspath(working_directory)
+    target_path_abs = os.path.abspath(full_path)
+
+    try:
+        if not target_path_abs.startswith(working_dir_abs):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        dir_name = os.path.dirname(target_path_abs)
+        if dir_name:
+            os.makedirs(dir_name, exist_ok=True)
+        with open(target_path_abs, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        error_message = str(e)
+        return f'Error: {error_message}'
--- tests.py
+++ tests.py
@@ -1,15 +1,15 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 if __name__ == "__main__":
-    print("Result for  calc / main test:")
-    print(get_file_content("calculator", "main.py"))
+    print("Result for  calc loremt.txt test:")
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 
     print("Result for calc / pkg test:")
-    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(write_file("calculator", "pkg/moerlorem.txt", "lorem ipsum dolor sit amet"))
 
-    print("Result for '/bin/cat' directory:")
-    print(get_file_content("calculator", "/bin/cat"))
+    print("Result for /tmp/temp.txt test:")
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
     #print("Result for '../' directory:")
     #print(get_files_info("calculator", "../"))
